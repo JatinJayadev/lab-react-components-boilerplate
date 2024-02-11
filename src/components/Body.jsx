59c010f37443a4035c5ad4d218f468578953d812
Jatin Jayadev <jatin.batchu@kalvium.community>
2024-02-11 10:26:43 +0530

    Finished Progression

---
+++ src/components/Body.jsx
@@ -0,0 +1,18 @@
+function Body(props) {
+    let imageData = props.data
+
+    return (
+        < div >
+            {
+                imageData.map(image => {
+                    return (
+                        <div key={image.id} className="column">
+                            <img src={image.img} alt=""></img>
+                        </div>
+                    )
+                })
+            }
+        </div >
+    )
+}
+export default Body;
